4c282e73508913e09ef5b3bf82ed591fc849f6bc
Hunter McNeil <huntermcneil041@gmail.com>
2022-05-11 15:10:04 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1>Under Construction :)</h1>
+      <div className="App-link">
+        <a href="mailto:huntermcneil041@gmail.com">Contact</a>
+      </div>
+    </header>
+  </div>
   );
 }
 
